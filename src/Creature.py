9039ac93a7d69898f14bedba535570d91e68c392
seandoo123 <seandoo123456@gmail.com>
2023-11-26 18:48:11 -0500

    added Creature.py

---
+++ src/Creature.py
@@ -0,0 +1,44 @@
+# Author: Hsiang-Mao Tsai
+# Author: Hsin-Yang Tu
+# Date: 11/18/2023
+# Description: creature class
+
+
+from FieldInhabitant import FieldInhabitant
+
+class Creature:
+    def __init__(self, symbol,x,y):
+        Creature.__init__(self,symbol)
+        self._x = x
+        self._y = y
+
+
+    def setX(self,x):
+        """
+        setter for coordinate x
+        :param x: new coordinate x
+        :return: none
+        """
+        self._x = x
+
+    def getX(self):
+        """
+        getter for coordinate x
+        :return: coordinate x
+        """
+        return self._x
+
+    def setY(self,y):
+        """
+        setter for coordinate y
+        :param y: new coordinate y
+        :return: none
+        """
+        self._y =y
+
+    def getY(self):
+        """
+        getter for coordinate y
+        :return: coordinate y
+        """
+        return self._y
