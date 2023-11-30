99e3a4e4c4650c4eca7b97fd309840ba6e6579f2
htsaii <a0955889206@gmail.com>
2023-11-30 04:50:02 -0500

    adding comments
    adding comments in GameEngine class.

--- src/Rabbit.py
+++ src/Rabbit.py
@@ -15,33 +15,3 @@ class Rabbit(Creature):
         :param col: col coordinate
         """
         Creature.__init__(self, "R", row, col)
-
-    # def setRow(self, row):
-    #     """
-    #     Set row coordinate
-    #     :param row: row coordinate
-    #     :return: none
-    #     """
-    #     self.__row = row
-
-    # def setCol(self, col):
-    #     """
-    #     Set col coordinate
-    #     :param col: col coordinate
-    #     :return: none
-    #     """
-    #     self.__col = col
-
-    # def getRow(self):
-    #     """
-    #     Get row coordinate
-    #     :return: row coordinate
-    #     """
-    #     return self.__row
-    
-    # def getCol(self):
-    #     """
-    #     Get col coordinate
-    #     :return: col coordinate
-    #     """
-    #     return self.__col
--- src/main.py
+++ src/main.py
@@ -6,24 +6,6 @@
 from GameEngine import GameEngine
 
 
-"""
-In a file named main.py, you should have:
-o Your main function in which:
-▪ You instantiate and store a GameEngine object
-▪ Initialize the game using the appropriate GameEngine function
-▪ Display the game’s introduction using the appropriate GameEngine function
-▪ Create an variable to store the number of remaining vegetables in the game, initialized
-using the appropriate GameEngine function
-▪ While there are still vegetables left in the game
-• Output the number of remaining vegetables and the player’s score
-• Print out the field using the appropriate GameEngine function
-• Move the rabbits using the appropriate GameEngine function
-• Move the captain using the appropriate GameEngine function
-• Determine the new number of remaining vegetables using the appropriate
-GameEngine function
-▪ Display the Game Over information using the appropriate GameEngine function
-▪ Handle the High Score functionality using the appropriate GameEngine function
-"""
 
 
 
@@ -32,9 +14,12 @@ def main():
     """
     Main function of the program
     """
+
+    # Initialize the game
     game = GameEngine()
     game.initializeGame()
 
+    # Start the game
     game.intro()
     numVeggies = game.remainingVeggies()
     while numVeggies > 0:
@@ -43,7 +28,8 @@ def main():
         game.moveRabbits()
         game.moveCaptain()
         numVeggies = game.remainingVeggies()
-        
+    
+    # End the game
     game.gameOver()
     game.highScore()
 
